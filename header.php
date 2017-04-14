42d741bacb857de09bedd53e234ddf6234a2120e
moslembusana <moslembusana@gmail.com>
2017-04-14 10:55:49 +0700

    revisi

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class='section2' id='header'>
+  <div class='widget Header' data-version='1' id='Header1'>
+    <div id='header-inner'>
+      
+      <div class='titlewrapper'>
+        <h1 class='title'>Facebook Marketing 2017</h1>
+      </div>
+      
+      <div class='descriptionwrapper'>
+        <p class='description'>
+          <span>Tools fenomenal yang Dapat Menggali Potensi "Harta Karun" Facebook, Menemukan  Calon Penonton Iklan Potensial tak terbatas Dalam Waktu yang Sangat Singkat! Dan telah digunakan oleh  4.400+ Pebisnis Online seperti Kamu...</span>
+        </p>
+      </div>
+
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -41,7 +41,9 @@
     <div class="container">
       <div class="row">
         <div class="one-half column">
-          <h4 class="hero-heading">Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
+          <?php
+            include 'header.php';  // postingan
+          ?>
           <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
         </div>
         <div class="one-half column phones">
@@ -76,17 +78,131 @@
 
   <div class="section get-help">
     <div class="container">
-      <h3 class="section-heading">Need help getting started?</h3>
-      <p class="section-description">Skeleton is an amazingly easy place to start with responsive development. If you want to learn more, just visit the documentation!</p>
-      <a class="button button-primary" href="http://getskeleton.com">View Skeleton Docs</a>
+      <h3 class="section-heading">Temukan penonton iklan facebook potensial dan tersembunyi untuk targeting berbagai produk dan segmen market bisnis kamu!</h3> 
     </div>
   </div>
+
+  <div class="section get-help">
+    <div class="container">
+      <div class="bagian1">
+      <h4>Pernahkah ketika kamu <b>beriklan di facebook</b> sering merasa seperti ini? :</h4>
+      <ul class="green-check1">
+        <li>Membutuhkan <b>waktu yang lama</b> untuk RISET siapa saja penonton iklan facebook yang tertarget...</li>
+        <li>Hanya menemukan calon penonton iklan tertarget (audience) dengan <b> jumlah yang sedikit... (Psst.., padahal ada audience tersembunyi)</b></li>
+        <li>Bingung setelah mengumpulkan audience, <b>siapa lagi yang bisa ditarget...</b></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="section values">
+    <div class="container">
+      <div class="row">
+        <div class="four columns">
+        <img class="img-responsive img-circle" src="http://insightzilla.com/wp-content/uploads/2016/02/photo.png"/>
+        <h5 class="value-heading"><b>Nofi Bayu Darmawan</b></h5>
+        <p class="kata_mereka"><a href="https://learn.hootsuite.com/u/nofibayu">Social Media Consultant</a>, Founder <a href="http://fanspage.co.id/">FanspageID</a> (Agency Facebook Marketing yang telah menghandle lebih dari 1000+ UKM dan Brands di Indonesia)</p>
+        </div>
+        <div class="eight columns">
+        <div class="kata_mereka2">
+        <h5>Assalamu`alaikum...,</h5>
+        <h5><b>
+        Salam hangat kepada seluruh Facebook Marketer di Indonesia dan Malaysia, disini aku mau share bagaimana pentingnya targeting audience iklan facebook dan mengapa kalian harus me-﻿remove﻿  masalah-masalah di atas!</b></h5>
+
+        <b>TARGETING</b> yang tepat.., menjadi poin penting dalam beriklan di facebook, bagaimana tidak? Dengan target akan lebih membuat iklan kamu :
+        <br/><br/>
+
+        <ul class="green-check1">
+          <li> Menjangkau <b>lebih banyak orang </b>dengan biaya yang murah...<br/></br>
+          Kenapa? Semakin <b> tepat targeting</b>, semakin banyak orang yang action pada iklan facebook kita (CTR), ketika semakin banyak result dalam performance facebook ads, maka daya jangkau iklan akan semakin luas dari biasanya....
+          </li>
+          <li>Lebih meningkatkan <b>konversi sales</b> karena targetnya tepat...</li>
+        </ul>
+
+
+        Kini kamu ga <b>bingung<b> lagi soal <b>TARGETING</b> iklan facebook, terlebih lagi, kamu dapat me-RISET Targeting dengan <b>sangat-sangat menghemat waktu....!</b> Boom!
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section get-help">
+    <div class="container">
+      <div class="bagian2">
+      <h4><b>Menghadirkan Special Untuk Kamu.....</b></h4>
+      <h5 align="center"><b>Dan sebelumnya telah digunakan oleh <span style="background-color: rgb(253, 249, 87);">4.400+</span>  Marketer dari Indonesia & Malaysia</b></h5>
+      <img src="http://insightzilla.com/wp-content/uploads/2016/02/cover-iz-3d.png"/>
+      <h4>Versi 3.0 <br> <span  style="background-color: rgb(255, 204, 0);">&nbsp;Lebih Powerfull </span> dari Versi Sebelumnya</h4>
+      <center><img src="http://insightzilla.com/wp-content/uploads/2016/02/Screen-Shot-2017-01-13-at-4.11.08-PM.png"/></center>
+      </div>
+    </div>
+  </div>
+
+   <div class="section get-help">
+    <div class="container">
+      <div class="bagian2">
+
+        <h5 align="center"><b>Simak Video Berikut Sampai Habis:</h5>
+        <h4><b>Semua Fitur InsightZilla v. 3.0 ?</b></h4>
+          <center><iframe width="640" height="360" src="https://www.youtube.com/embed/4avIAtnGhBU?controls=0" frameborder="0" allowfullscreen></iframe></center>
+        <h5 align="center"><b>Jika Kamu Masih PEMULA di Facebook Advertising, dan belum bisa 100% memahami seluruh fitur di atas, 2 Video Ini Akan Meyakinkan Kamu Betapa Kamu Butuh InsightZilla! Namun jika sudah biasa menggunakan Audience Insight, abaikan 2 video ini!</b></h5>
+
+        
+
+      </div>
+      <div class="six columns"><h5><b>Video 1</b> (Pengenalan Targeting Iklan Facebook Basic)</h5>
+          <iframe width="100%" height="262" src="https://www.youtube.com/embed/cC6va5JFhoA" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="six columns"><h5><b>Video 2</b> (Riset Targeting Interest dengan Audience Insight secara Manual)</h5>
+          <iframe width="466" height="262" src="https://www.youtube.com/embed/tq8C4npFDWg" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+  </div>
+
 
   <div class="section get-help">
     <div class="container">
       <?php
       include 'postingan.php';  // postingan
       ?>
+    </div>
+  </div>
+
+  <div class="section categories">
+    <div class="container">
+      <h3 class="section-heading">Responsive Images</h3>
+      <p class="section-description">Skeleton images sit easily in grid with .u-max-full-width class. I suggest exploring solution to serving different images based on device size.</p>
+      <div class="row">
+        <div class="one-half column category">
+          <img class="u-max-full-width" src="http://insightzilla.com/wp-content/uploads/2016/02/ezgif.com-video-to-gif-19.gif">
+        </div>
+        <div class="one-half column category">
+
+        <div class="kata_mereka2">
+          <ul class="green-check1">
+            <li> Menjangkau <b>lebih banyak orang </b>dengan biaya yang murah...<br/></br>
+            Kenapa? Semakin <b> tepat targeting</b>, semakin banyak orang yang action pada iklan facebook kita (CTR), ketika semakin banyak result dalam performance facebook ads, maka daya jangkau iklan akan semakin luas dari biasanya....
+            </li>
+            <li>Lebih meningkatkan <b>konversi sales</b> karena targetnya tepat...</li>
+          </ul>
+        </div>
+         1. Kamu Ga Lagi Bingung Bagaimana Menggali Ide Targeting Facebook Ads dengan Hadirnya InsightZilla....
+
+Mengapa? Kamu perlu membaca lanjutannya hingga akhir, sehingga kamu tahu mengapa harus menggunakan InsightZilla....
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="one-half column category">
+          <img class="u-max-full-width" src="http://insightzilla.com/wp-content/uploads/2016/02/ezgif.com-video-to-gif-19.gif">
+        </div>
+        <div class="one-half column category">
+          <img class="u-max-full-width" src="images/placeholder.png">
+        </div>
+      </div>
+
     </div>
   </div>
 
@@ -104,6 +220,8 @@
       </div>
     </div>
   </div>
+
+
 
 
 

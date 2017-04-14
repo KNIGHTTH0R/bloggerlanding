2986dbfe2a8f88707b71ce3111a762dbb928e610
moslembusana <moslembusana@gmail.com>
2017-04-14 07:07:36 +0700

    penambahan struktur postingan

---
+++ postingan.php
@@ -0,0 +1,51 @@
+<div class="date-outer">
+<div class="date-posts">
+	<div class='post-outer'>
+		<div class='post hentry uncustomized-post-template' itemprop='blogPost' itemscope='itemscope' itemtype='http://schema.org/BlogPosting'>
+			<meta content='345165601197112860' itemprop='blogId'/>
+			<meta content='1632747976599459570' itemprop='postId'/>
+			<a name='1632747976599459570'></a>
+			<h3 class='post-title entry-title' itemprop='name'>
+			<a href='http://insightzilla-facebook.blogspot.co.id/2017/04/adalah-toko-online-busana-muslim-pria.html'>adalah toko online busana muslim pria</a>
+			</h3>
+		
+			<div class='post-header'>
+				<div class='post-header-line-1'></div>
+			</div>
+
+			<div class='post-body entry-content' id='post-body-1632747976599459570' itemprop='description articleBody'>
+				<div dir="ltr" style="text-align: left;" trbidi="on">
+				moslembusana.com - adalah toko online busana muslim wanita, busana 
+				muslim pria dan  busana muslim anak, moslembusana.com berlokasi di 
+				Tasikmalaya Jawa Barat. Saat ini kami belum memiliki toko offline 
+				mudah-mudah untuk kedepannya kami bisa memilikinya aamiin. Produk yang 
+				kami jual dikirim dari beberapa kota besar diantaranya dari kota Depok, 
+				Bandung, Jakarta dan daerah lainnya. Kami menawarkan produk busana 
+				</div>
+				<div style='clear: both;'></div>
+			</div>
+
+			<div class='post-footer'>
+				<div class='post-footer-line post-footer-line-1'>
+					<span class='post-author vcard'></span>
+					<span class='post-timestamp'></span>
+					<span class='reaction-buttons'></span>
+					<span class='post-comment-link'></span>
+					<span class='post-backlinks post-comment-link'></span>
+					<span class='post-icons'></span>
+					<div class='post-share-buttons goog-inline-block'></div>
+				</div>
+
+				<div class='post-footer-line post-footer-line-2'>
+					<span class='post-labels'></span>
+				</div>
+
+				<div class='post-footer-line post-footer-line-3'>
+					<span class='post-location'></span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+</div>
